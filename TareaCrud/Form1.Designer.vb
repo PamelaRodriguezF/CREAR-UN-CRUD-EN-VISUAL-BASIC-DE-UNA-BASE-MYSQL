6a65b432c28106af3e37d8614f7834e5944fcffb
Pamela Rodriguez <63885500+PamelaRodriguezF@users.noreply.github.com>
2020-05-13 11:37:51 -0300

    Add files via upload

---
+++ TareaCrud/Form1.Designer.vb
@@ -0,0 +1,176 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.VisorDeDatos = New System.Windows.Forms.DataGridView()
+        Me.textID = New System.Windows.Forms.Label()
+        Me.AgregarRegistro = New System.Windows.Forms.Button()
+        Me.CampoId = New System.Windows.Forms.TextBox()
+        Me.NombreMascotaCampo = New System.Windows.Forms.TextBox()
+        Me.TextNombre = New System.Windows.Forms.Label()
+        Me.RazaCampo = New System.Windows.Forms.TextBox()
+        Me.TextRaza = New System.Windows.Forms.Label()
+        Me.CampoColor = New System.Windows.Forms.TextBox()
+        Me.TextColor = New System.Windows.Forms.Label()
+        Me.EditarRegistro = New System.Windows.Forms.Button()
+        Me.EliminarRegistro = New System.Windows.Forms.Button()
+        CType(Me.VisorDeDatos, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'VisorDeDatos
+        '
+        Me.VisorDeDatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.VisorDeDatos.Location = New System.Drawing.Point(262, 23)
+        Me.VisorDeDatos.Name = "VisorDeDatos"
+        Me.VisorDeDatos.Size = New System.Drawing.Size(526, 359)
+        Me.VisorDeDatos.TabIndex = 0
+        '
+        'textID
+        '
+        Me.textID.AutoSize = True
+        Me.textID.Location = New System.Drawing.Point(12, 30)
+        Me.textID.Name = "textID"
+        Me.textID.Size = New System.Drawing.Size(18, 13)
+        Me.textID.TabIndex = 1
+        Me.textID.Text = "ID"
+        '
+        'AgregarRegistro
+        '
+        Me.AgregarRegistro.Location = New System.Drawing.Point(15, 235)
+        Me.AgregarRegistro.Name = "AgregarRegistro"
+        Me.AgregarRegistro.Size = New System.Drawing.Size(241, 23)
+        Me.AgregarRegistro.TabIndex = 2
+        Me.AgregarRegistro.Text = "Agregar Registro"
+        Me.AgregarRegistro.UseVisualStyleBackColor = True
+        '
+        'CampoId
+        '
+        Me.CampoId.Location = New System.Drawing.Point(81, 23)
+        Me.CampoId.Name = "CampoId"
+        Me.CampoId.Size = New System.Drawing.Size(175, 20)
+        Me.CampoId.TabIndex = 3
+        '
+        'NombreMascotaCampo
+        '
+        Me.NombreMascotaCampo.Location = New System.Drawing.Point(81, 65)
+        Me.NombreMascotaCampo.Name = "NombreMascotaCampo"
+        Me.NombreMascotaCampo.Size = New System.Drawing.Size(175, 20)
+        Me.NombreMascotaCampo.TabIndex = 5
+        '
+        'TextNombre
+        '
+        Me.TextNombre.AutoSize = True
+        Me.TextNombre.Location = New System.Drawing.Point(12, 72)
+        Me.TextNombre.Name = "TextNombre"
+        Me.TextNombre.Size = New System.Drawing.Size(44, 13)
+        Me.TextNombre.TabIndex = 4
+        Me.TextNombre.Text = "Nombre"
+        '
+        'RazaCampo
+        '
+        Me.RazaCampo.Location = New System.Drawing.Point(81, 107)
+        Me.RazaCampo.Name = "RazaCampo"
+        Me.RazaCampo.Size = New System.Drawing.Size(175, 20)
+        Me.RazaCampo.TabIndex = 7
+        '
+        'TextRaza
+        '
+        Me.TextRaza.AutoSize = True
+        Me.TextRaza.Location = New System.Drawing.Point(12, 114)
+        Me.TextRaza.Name = "TextRaza"
+        Me.TextRaza.Size = New System.Drawing.Size(32, 13)
+        Me.TextRaza.TabIndex = 6
+        Me.TextRaza.Text = "Raza"
+        '
+        'CampoColor
+        '
+        Me.CampoColor.Location = New System.Drawing.Point(81, 149)
+        Me.CampoColor.Name = "CampoColor"
+        Me.CampoColor.Size = New System.Drawing.Size(175, 20)
+        Me.CampoColor.TabIndex = 9
+        '
+        'TextColor
+        '
+        Me.TextColor.AutoSize = True
+        Me.TextColor.Location = New System.Drawing.Point(12, 156)
+        Me.TextColor.Name = "TextColor"
+        Me.TextColor.Size = New System.Drawing.Size(31, 13)
+        Me.TextColor.TabIndex = 8
+        Me.TextColor.Text = "Color"
+        '
+        'EditarRegistro
+        '
+        Me.EditarRegistro.Location = New System.Drawing.Point(134, 264)
+        Me.EditarRegistro.Name = "EditarRegistro"
+        Me.EditarRegistro.Size = New System.Drawing.Size(122, 23)
+        Me.EditarRegistro.TabIndex = 10
+        Me.EditarRegistro.Text = "Editar"
+        Me.EditarRegistro.UseVisualStyleBackColor = True
+        '
+        'EliminarRegistro
+        '
+        Me.EliminarRegistro.Location = New System.Drawing.Point(15, 264)
+        Me.EliminarRegistro.Name = "EliminarRegistro"
+        Me.EliminarRegistro.Size = New System.Drawing.Size(113, 23)
+        Me.EliminarRegistro.TabIndex = 11
+        Me.EliminarRegistro.Text = "Eliminar"
+        Me.EliminarRegistro.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.EliminarRegistro)
+        Me.Controls.Add(Me.EditarRegistro)
+        Me.Controls.Add(Me.CampoColor)
+        Me.Controls.Add(Me.TextColor)
+        Me.Controls.Add(Me.RazaCampo)
+        Me.Controls.Add(Me.TextRaza)
+        Me.Controls.Add(Me.NombreMascotaCampo)
+        Me.Controls.Add(Me.TextNombre)
+        Me.Controls.Add(Me.CampoId)
+        Me.Controls.Add(Me.AgregarRegistro)
+        Me.Controls.Add(Me.textID)
+        Me.Controls.Add(Me.VisorDeDatos)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.VisorDeDatos, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents VisorDeDatos As DataGridView
+    Friend WithEvents textID As Label
+    Friend WithEvents AgregarRegistro As Button
+    Friend WithEvents CampoId As TextBox
+    Friend WithEvents NombreMascotaCampo As TextBox
+    Friend WithEvents TextNombre As Label
+    Friend WithEvents RazaCampo As TextBox
+    Friend WithEvents TextRaza As Label
+    Friend WithEvents CampoColor As TextBox
+    Friend WithEvents TextColor As Label
+    Friend WithEvents EditarRegistro As Button
+    Friend WithEvents EliminarRegistro As Button
+End Class
---
+++ TareaCrud/Form1.vb
@@ -0,0 +1,136 @@
+﻿Imports MySql.Data.MySqlClient
+
+Public Class Form1
+    Dim conexion As MySqlConnection = New MySqlConnection
+    Dim cmd As New MySqlCommand
+    Public Property Obj As Object
+
+    Public Sub TbColor()
+        ActualizarSelect()
+    End Sub
+
+    Private Sub AgregarRegistro_Click(sender As Object, e As EventArgs) Handles AgregarRegistro.Click
+        conexion.ConnectionString = "Server='localhost';Database='veterinaria';Uid=root;Pwd='';"
+
+        Try
+            conexion.Open()
+            cmd.Connection = conexion
+
+            cmd.CommandText = "INSERT INTO perros (nombre_mascota,raza,color) VALUES(@nombre_mascota,@raza,@color)"
+            cmd.Prepare()
+
+            cmd.Parameters.Clear()
+            cmd.Parameters.AddWithValue("@nombre_mascota", NombreMascotaCampo.Text)
+            cmd.Parameters.AddWithValue("@raza", RazaCampo.Text)
+            cmd.Parameters.AddWithValue("@color", CampoColor.Text)
+            cmd.ExecuteNonQuery()
+            NombreMascotaCampo.Clear()
+            RazaCampo.Clear()
+            CampoId.Clear()
+
+            conexion.Close()
+
+            ' MsgBox("Ah ingresado un nueva mascota ")
+            ActualizarSelect()
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Sub ActualizarSelect()
+        Dim ds As DataSet = New DataSet
+        Dim adaptador As MySqlDataAdapter = New MySqlDataAdapter
+
+        conexion.ConnectionString = "Server='localhost';Database='veterinaria';Uid=root;Pwd='';"
+
+
+        Try
+            conexion.Open()
+            cmd.Connection = conexion
+
+            cmd.CommandText = "SELECT * FROM perros ORDER BY id ASC"
+            adaptador.SelectCommand = cmd
+            adaptador.Fill(ds, "Tabla")
+            VisorDeDatos.DataSource = ds
+            VisorDeDatos.DataMember = "Tabla"
+
+
+            conexion.Close()
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+
+
+    End Sub
+
+    Private Sub TbColor_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ActualizarSelect()
+    End Sub
+
+    Private Sub VisorDeDatos_SelectionChanged(sender As Object, e As EventArgs) Handles VisorDeDatos.SelectionChanged
+        If VisorDeDatos.SelectedRows.Count > 0 Then
+            NombreMascotaCampo.Text = VisorDeDatos.Item("nombre_mascota", VisorDeDatos.SelectedRows(0).Index).Value
+            RazaCampo.Text = VisorDeDatos.Item("raza", VisorDeDatos.SelectedRows(0).Index).Value
+            CampoId.Text = VisorDeDatos.Item("color", VisorDeDatos.SelectedRows(0).Index).Value
+            CampoId.Text = VisorDeDatos.Item("id", VisorDeDatos.SelectedRows(0).Index).Value
+        End If
+    End Sub
+
+    Private Sub EditarRegistro_Click(sender As Object, e As EventArgs) Handles EditarRegistro.Click
+        conexion.ConnectionString = "Server='localhost';Database='veterinaria';Uid=root;Pwd='';"
+
+        Try
+            conexion.Open()
+            cmd.Connection = conexion
+            cmd.CommandText = "UPDATE perros SET nombre_mascota=@nombre_mascota, raza=@raza, color=@color WHERE id=@id"
+            cmd.Prepare()
+
+            cmd.Parameters.Clear()
+            cmd.Parameters.AddWithValue("@nombre_mascota", NombreMascotaCampo.Text)
+            cmd.Parameters.AddWithValue("@raza", RazaCampo.Text)
+            cmd.Parameters.AddWithValue("@color", CampoColor.Text)
+            cmd.Parameters.AddWithValue("@id", CampoId.Text)
+            cmd.ExecuteNonQuery()
+            NombreMascotaCampo.Clear()
+            RazaCampo.Clear()
+            CampoColor.Clear()
+            CampoId.Clear()
+            conexion.Close()
+
+            ActualizarSelect()
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub EliminarRegistro_Click(sender As Object, e As EventArgs) Handles EliminarRegistro.Click
+        conexion.ConnectionString = "Server='localhost';Database='veterinaria';Uid=root;Pwd='';"
+        Try
+            conexion.Open()
+            cmd.Connection = conexion
+            If MessageBox.Show("seguro de realizar esta accion", "confirmacion", MessageBoxButtons.OKCancel, MessageBoxIcon.Question, MessageBoxDefaultButton.Button1) = DialogResult.OK Then
+
+            End If
+            cmd.CommandText = "Delete from perros where id = @id"
+            cmd.Prepare()
+
+            cmd.Parameters.Clear()
+            cmd.Parameters.AddWithValue("@id", CampoId.Text)
+            cmd.ExecuteNonQuery()
+            CampoId.Clear()
+
+
+            conexion.Close()
+
+            ActualizarSelect()
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+
+        'Pamela Rodriguez
+    End Sub
+End Class
